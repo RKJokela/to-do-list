f459e9af11af09340199de12babd57708f2f6bd1
RKJokela <randyjokela@gmail.com>
2015-08-27 22:37:57 -0700

    Intercept back button in AddTaskActivity
    This was causing a crash when back button pressed. Instead, it should just function the same as cancel button.

--- app/src/main/java/com/rjokela/todolist/AddTaskActivity.java
+++ app/src/main/java/com/rjokela/todolist/AddTaskActivity.java
@@ -2,11 +2,20 @@ package com.rjokela.todolist;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class AddTaskActivity extends ActionBarActivity {
+
+    @Override
+    public void onBackPressed() {
+        Log.d(AddTaskActivityFragment.TAG, "onBackPressed() called, overridden to mimic cancel button");
+        AddTaskActivityFragment fragment =
+                (AddTaskActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_add_task);
+        fragment.cancelButtonClick();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
